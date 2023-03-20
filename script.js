2e900706f9dd9d43b45f59bbf85d52ab1f5f832a
giorgisilagadze <silagadze.gio123@gmail.com>
2023-03-20 19:46:15 +0400

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("input");
+const body = document.querySelector("body");
+
+
+input.addEventListener("keydown", (event) => {
+    if (event.code === "Enter") {
+        const newTodo = document.createElement("div");
+        newTodo.classList.add("")
+    }
+})
